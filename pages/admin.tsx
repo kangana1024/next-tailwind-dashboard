28a53f36eaa6f5e986aa0bdeaaf1520ff1e6035e
Kanchana Sondee <kangana512@gmail.com>
2021-01-17 22:40:00 +0700

    change chart

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,10 +1,28 @@
 import DashboardLayout from '../components/layouts/admin'
 import Head from 'next/head'
 import React from 'react'
-import { VictoryPie } from 'victory'
+import {
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  Legend,
+  BarChart,
+  Bar,
+  CartesianGrid
+} from "recharts"
+
+const data = [
+  { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
+  { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
+  { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
+  { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
+  { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
+  { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
+  { name: "Page G", uv: 3490, pv: 4300, amt: 2100 }
+];
 
 const Layout = () => {
-
   return (
     <>
       <Head>
@@ -86,7 +104,17 @@ const Layout = () => {
 
             <div className="w-full px-6 sm:w-1/2 xl:w-1/3 mt-6">
               <div className="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
-                <VictoryPie />
+                <ResponsiveContainer width="100%" height={200}>
+                  <BarChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="pv" fill="#8884d8" />
+                    <Bar dataKey="uv" fill="#82ca9d" />
+                  </BarChart>
+                </ResponsiveContainer>
               </div>
             </div>
           </div>
