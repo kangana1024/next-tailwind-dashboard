c436e2bdd1a02f61754533aeb71987121396cfb6
Kanchana Sondee <kangana512@gmail.com>
2021-01-17 23:26:23 +0700

    change chart

--- components/layouts/admin.tsx
+++ components/layouts/admin.tsx
@@ -13,53 +13,55 @@ const DashboardLayout = (props: any) => {
     setActiveMenu(false)
   }
   return (
-    <DashboardSettingProvider value={{
-      loading: {
-        loading,
-        setLoading
-      },
-      menu: {
-        activeMenu,
-        setActiveMenu
-      },
-      version: {
-        version,
-        setVersion
-      }
-    }} >
-      <div className="flex h-screen bg-gray-200">
-        <div className="flex">
-          <div onClick={hiddenSidbar} className={"fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden" + (activeMenu ? " block" : " hidden")}></div>
-          <div className={"fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0" + (activeMenu ? " translate-x-0 ease-out" : " -translate-x-full ease-in")}>
-            <div className="flex items-center justify-center mt-8">
-              <div className="flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path fill="#4f46e5" d="M0 0h24v24H0z"></path>
-                  <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"></path>
-                </svg>
-                <span className="text-white text-2xl mx-2 font-semibold">Showkhun CMS</span>
+    <>
+      <DashboardSettingProvider value={{
+        loading: {
+          loading,
+          setLoading
+        },
+        menu: {
+          activeMenu,
+          setActiveMenu
+        },
+        version: {
+          version,
+          setVersion
+        }
+      }} >
+        <div className="flex h-screen bg-gray-200">
+          <div className="flex">
+            <div onClick={hiddenSidbar} className={"fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden" + (activeMenu ? " block" : " hidden")}></div>
+            <div className={"fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0" + (activeMenu ? " translate-x-0 ease-out" : " -translate-x-full ease-in")}>
+              <div className="flex items-center justify-center mt-8">
+                <div className="flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path fill="#4f46e5" d="M0 0h24v24H0z"></path>
+                    <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"></path>
+                  </svg>
+                  <span className="text-white text-2xl mx-2 font-semibold">Showkhun CMS</span>
+                </div>
               </div>
-            </div>
 
-            <NavBar />
+              <NavBar />
+            </div>
+          </div>
+
+          <div className="flex-1 flex flex-col overflow-hidden">
+            <HeaderLayout />
+            <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+              <div className="container mx-auto px-6 py-8">
+                {props.children}
+              </div>
+            </main>
           </div>
         </div>
-
-        <div className="flex-1 flex flex-col overflow-hidden">
-          <HeaderLayout />
-          <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
-            <div className="container mx-auto px-6 py-8">
-              {props.children}
-            </div>
-          </main>
-        </div>
-      </div>
-    </DashboardSettingProvider>
+      </DashboardSettingProvider>
+    </>
   )
 }
 
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,32 +1,19 @@
 import DashboardLayout from '../components/layouts/admin'
 import Head from 'next/head'
 import React from 'react'
-import {
-  XAxis,
-  YAxis,
-  Tooltip,
-  ResponsiveContainer,
-  Legend,
-  BarChart,
-  Bar,
-  CartesianGrid
-} from "recharts"
+import dynamic from 'next/dynamic';
 
-const data = [
-  { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
-  { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
-  { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
-  { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
-  { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
-  { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
-  { name: "Page G", uv: 3490, pv: 4300, amt: 2100 }
-];
 
 const Layout = () => {
   return (
     <>
       <Head>
         <title>Dashboard Admin</title>
+        <meta property="og:url" content="https://demoadmin.showkhun.co/admin" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Demo Admin Nextjs 10 + Tailwind" />
+        <meta property="og:description" content="Nextjs 10 + Tailwind And Preact render" />
+        <meta property="og:image" content="https://demoadmin.showkhun.co/images/admindashboard.png" />
       </Head>
       <DashboardLayout>
         <h3 className="text-gray-700 text-3xl font-medium">Dashboard</h3>
@@ -104,17 +91,7 @@ const Layout = () => {
 
             <div className="w-full px-6 sm:w-1/2 xl:w-1/3 mt-6">
               <div className="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
-                <ResponsiveContainer width="100%" height={200}>
-                  <BarChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="pv" fill="#8884d8" />
-                    <Bar dataKey="uv" fill="#82ca9d" />
-                  </BarChart>
-                </ResponsiveContainer>
+                <GraphDemo width={200} height={300} />
               </div>
             </div>
           </div>
@@ -123,5 +100,8 @@ const Layout = () => {
     </>
   )
 }
+const GraphDemo = dynamic(() => import('../components/layouts/demograph'), {
+  ssr: false,
+});
 
 export default Layout
